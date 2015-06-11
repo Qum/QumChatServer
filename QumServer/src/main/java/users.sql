14a444b2860edcbab25937e5acd6e2ed9bc075e3
Qum <kyudyazogajaz@meta.ua>
2015-06-11 09:37:53 +0300

    add user sql file

---
+++ QumServer/src/main/java/users.sql
@@ -0,0 +1,14 @@
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(20) NOT NULL,
+  `pass` varchar(256) NOT NULL,
+  `acc_lvl` tinyint(4) NOT NULL,
+  `last_ip` varchar(20) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
